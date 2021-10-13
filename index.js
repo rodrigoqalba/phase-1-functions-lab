22fb7974d2f3adb50ed6adca8ad9abfdca6e5d85
Rodrigo Alba <rodrigoqalba@gmail.com>
2021-10-13 09:35:44 -0400

    commit tree adb2ecd7e5e0bbb494db515e6ee77ee501b98d3e

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
 // Code your solution in this file!
+const hq = 42;
+const feetInBlock = 264
+
+function distanceFromHqInBlocks(numOfBlocks) {
+    if (numOfBlocks > 42) {
+        return numOfBlocks - hq;
+    } else (numOfBlocks < 42) 
+        return hq - numOfBlocks;  
+}
+
+function distanceFromHqInFeet(numOfBlocks) {
+    return distanceFromHqInBlocks(numOfBlocks) * feetInBlock;
+}
+
+function distanceTravelledInFeet(num1, num2) {
+    if (num1 > num2) {
+        return  (num1 - num2) * feetInBlock
+} else (num1 < num2)
+    return (num2 - num1) * feetInBlock
+}
+
+function calculatesFarePrice(num1, num2) {
+    if (distanceTravelledInFeet(num1, num2) <= 400) {
+        return 0
+    } else if (distanceTravelledInFeet(num1, num2) > 400 && distanceTravelledInFeet(num1, num2) <= 2000) {
+        return (distanceTravelledInFeet(num1, num2) -400) * 0.02;
+    } else if (distanceTravelledInFeet(num1, num2) > 2000 && distanceTravelledInFeet(num1, num2) <2500) {
+        return 25
+    } else if (distanceTravelledInFeet(num1, num2) >= 25)
+        return 'cannot travel that far'
+}
